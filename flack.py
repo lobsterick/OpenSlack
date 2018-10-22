d98f4afa9bccaefec30a1a2d17fcbdca85af5c88
lobsterick <lobsterick@gmail.com>
2018-10-23 01:13:26 +0200

    Some JS improvements - working JS add room and dynamic change of room_list

--- flack.py
+++ flack.py
@@ -1,22 +1,25 @@
-from flask import Flask, session, request, render_template, redirect, url_for, jsonify
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
-import random, string
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-app.secret_key = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
-print(app.secret_key)
+socketio = SocketIO(app)
 
 global nickname_list, rooms_list
 nickname_list = []
-rooms_list = ["General", "Room_first", "Test_room"]
+rooms_list = ["General", "Room_first", "Room_second", "Room_third"]
 last_room = {}
+
 
 @app.route('/')
 def home():
-    return render_template("index.html", room_now="General", rooms_list=rooms_list)
+    if session.get("logged_in"):
+        return render_template("room.html", room_now=session["last_room"], rooms_list=rooms_list, nickname = session["nickname"])
+    else:
+        return render_template("index.html", rooms_list=rooms_list)
 
 
 @app.route('/login', methods=['POST'])
@@ -28,7 +31,8 @@ def do_admin_login():
         session['logged_in'] = True
         session["nickname"] = nickname
         nickname_list.append(nickname)
-        return render_template("index.html", info_type="succes", info_text=f"You are logged as {nickname}", room_now="General", rooms_list=rooms_list)
+        print(f"{nickname} logged in :)")
+        return render_template("room.html", info_type="succes", info_text=f"You are logged as {nickname}", room_now="General", rooms_list=rooms_list, nickname=nickname)
 
 
 @app.route("/logout")
@@ -37,19 +41,46 @@ def logout():
 
         # In case there was a server restart when user is still logged in and - in this case - his nickname is not in the list anymore (list of nicknames is stored only when SERVER IS WORKING)
         # In this case, we trust that user is not manipulating cookies, but still - we don't check that nick is associated with the same person whatsoever...
+        exituser=session["nickname"]
         if session["nickname"] in nickname_list:
             nickname_list.remove(session["nickname"])
 
         session.clear()
-        print("Left users:", nickname_list)
-        return render_template("index.html", info_type="neutral", info_text="Hope to see you soon! :)")
+        print(f"{exituser} logged out :(")
+        return render_template("index.html", info_type="neutral", info_text="Hope to see you soon! :)", rooms_list=rooms_list)
     else:
         return redirect(url_for('home'))
 
 
 @app.route("/room/<roomname>")
 def gotoroom(roomname):
-    return render_template("index.html", room_now=roomname, rooms_list=rooms_list)
+    if session.get('logged_in'):
+        session["last_room"] = roomname
+        if roomname in rooms_list:
+            return render_template("room.html", room_now=roomname, rooms_list=rooms_list, nickname=session["nickname"])
+        else:
+            return render_template("room.html", info_type="error", info_text="Room doesn't exits!", nickname=session["nickname"])
+    else:
+        return render_template("index.html", info_type="error", info_text="You must first choose nickname!")
+
+
+@app.route("/new_room", methods=['POST'])
+def new_room():
+    if session.get('logged_in'):
+        new_room_name = request.form["new_room_name"]
+        return redirect(url_for('gotoroom', roomname=new_room_name))
+    else:
+        return render_template("index.html", info_type="error", info_text="You must first choose nickname!")
+
+
+@socketio.on("update_room_list")
+def add_room(new_room_name):
+    if not new_room_name in rooms_list:
+        rooms_list.append(new_room_name)
+        emit("Update room list", new_room_name, broadcast=True)
+        print(f"NOWY POKÓJ {new_room_name}")
+    else:
+        render_template("index.html", info_type="error", info_text="This room exist!")
 
 
 if __name__ == '__main__':
---
+++ static/layout.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+
+    // When you add new room with submit, server should update it's table of rooms
+
+    socket.on('connect', () => {
+        document.querySelector("#new_room_form").onsubmit = function () {
+            const new_room_name = document.querySelector("#new_room_name").value;
+            socket.emit("update_room_list", new_room_name)
+        }
+    });
+
+    // When a new room is announced, add to the dropdown rooms list
+    socket.on('Update room list', data => {
+        var dropdownlist = document.getElementById("dropdown_rooms_list");
+        var opt = document.createElement('option');
+        opt.innerHTML = data;
+        opt.value = `/room/${data}`;
+        dropdownlist.appendChild(opt)
+    });
+});
+
+
+
