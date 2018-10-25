e245cdefff7aeb262bbf6c8020844c28209df7dd
lobsterick <lobsterick@gmail.com>
2018-10-26 01:00:44 +0200

    Half-done backend for all-dynamic-JS add and get messages (problem with parsing etc)
    Still problem with jumping site when click on dropdownlist

--- flack.py
+++ flack.py
@@ -1,6 +1,7 @@
 from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from flask_socketio import SocketIO, emit
+import time
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
@@ -8,24 +9,22 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 socketio = SocketIO(app)
 
-
-global nickname_list, rooms_list, messages_list
 nickname_list = []
-rooms_list = ["General", "Room_first", "Room_second", "Room_third"]
-messages_list = {"General": list(), "Room_first": list(), "Room_second": list(), "Room_third": list()}
+rooms_list = ["General", "Room_first"]
+# TODO unify getting rooms_list from message_list
+messages_list = {"General":
+                     [[1, "admin", "This is first message, that appears with timestamp of one"],
+                      [2, "admin", "This is second message, that appears with timestamp of two"],
+                      [3, "admin", "This is third message, that appears with timestamp of three"]], "Room_first": []}
 
 app.jinja_env.globals['messages_list'] = messages_list
-
-
-for key in messages_list:
-    for i in range(50):
-        messages_list[key].append(f"Message number {i} from room {key} asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd asd ")
+# TODO add all stuff from render_template to free all return render_template from repeated parameters
 
 
 @app.route('/')
 def home():
     if session.get("logged_in"):
-        return render_template("room.html", room_now=session["last_room"], rooms_list=rooms_list, nickname = session["nickname"])
+        return render_template("room.html", room_now=session["last_room"], rooms_list=rooms_list, nickname=session["nickname"])
     else:
         return render_template("index.html", rooms_list=rooms_list)
 
@@ -49,7 +48,7 @@ def logout():
 
         # In case there was a server restart when user is still logged in and - in this case - his nickname is not in the list anymore (list of nicknames is stored only when SERVER IS WORKING)
         # In this case, we trust that user is not manipulating cookies, but still - we don't check that nick is associated with the same person whatsoever...
-        exituser=session["nickname"]
+        exituser = session["nickname"]
         if session["nickname"] in nickname_list:
             nickname_list.remove(session["nickname"])
 
@@ -76,6 +75,7 @@ def gotoroom(roomname):
 def new_room():
     if session.get('logged_in'):
         new_room_name = request.form["new_room_name"]
+
         return redirect(url_for('gotoroom', roomname=new_room_name))
     else:
         return render_template("index.html", info_type="error", info_text="You must first choose nickname!")
@@ -85,6 +85,7 @@ def new_room():
 def add_room(new_room_name):
     if not new_room_name in rooms_list:
         rooms_list.append(new_room_name)
+        messages_list.update({new_room_name: {}})
         emit("Update room list", new_room_name, broadcast=True)
         print(f"NOWY POKÓJ {new_room_name}")
     else:
@@ -93,9 +94,15 @@ def add_room(new_room_name):
 
 @socketio.on("new_message_submit")
 def add_message(new_message_body):
-    rooms_list.append(new_room_name)
-    emit("update_messages", new_message_body, broadcast=True)
-    print(f"NOWA WIADOMOŚĆ {new_message_body}")
+    messages_list_in_room = messages_list[session["last_room"]]
+
+    messages_list_in_room.update({"author": session["nickname"], "datetime": time.time(), "message": new_message_body})
+    # TODO repair parsing json for JS
+
+    messages_list_in_room = sorted(messages_list_in_room[session["last_room"]])
+
+    emit("update_messages", messages_list_in_room, broadcast=True)
+    print(f"NOWA WIADOMOŚĆ {new_message_body} w pokoju {room}")
 
 
 if __name__ == '__main__':
--- static/room.js
+++ static/room.js
@@ -1,16 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     window.scrollTo(0,document.body.scrollHeight);
+
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
 
-    // When you add new room with submit, server should update it's table of rooms
+    // When you add new message with submit, server should append it to the room's messages
 
     socket.on('connect', () => {
         document.querySelector("#new_message").onsubmit = function () {
-            const new_message_body = document.querySelector("#new_message_body").value;
-            socket.emit("new_message_submit", new_message_body)
+            var new_message_body = document.querySelector("#new_message_body").value;
+            socket.emit("new_message_submit", new_message_body);
+            document.querySelector("#new_message_body").value = '';
+            return false
         }
     });
+
+
+        // When a new message is announced, reload all messages on page
+    socket.on('update_messages', updated_list=> {
+        var last_room = window.location.href.split("/").pop();
+        if (updated_list["room"] === last_room){
+            var messages_last_100 = document.getElementById("messages_last_100");
+            messages_last_100.innerHTML = "";
+            var posts;
+            // console.log(updated_list); // debugging
+            for (posts in updated_list["message_list"])
+            {
+                var post = document.createElement('div');
+                var inHTML = '<div class="post"><span style=\'color: #946e09; font-weight:bold\'>';
+                inHTML += "USERNAME";
+                inHTML += "</span> on date XXX said: </br>";
+                inHTML += updated_list["message_list"][posts];
+                inHTML += "</div>";
+                post.innerHTML = inHTML;
+                messages_last_100.appendChild(post);
+            }
+            window.scrollTo(0,document.body.scrollHeight);
+            return false
+    }});
+
 });
+
+
